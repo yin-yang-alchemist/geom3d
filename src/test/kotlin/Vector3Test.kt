026f07de8b649435c3a8a21b62e6793233ad47d7
yin-yang-alchemist <nakamoto.masa@gmail.com>
2019-02-27 08:44:37 +0900

    Vector3のテストメソッドをテスト項目ごとに分割し、AssertJで書き換え

--- src/test/kotlin/Vector3Test.kt
+++ src/test/kotlin/Vector3Test.kt
@@ -10,17 +10,17 @@ internal class Vector3Test {
     /** 許容値 */
     private val TOL = 1e-9
 
-    /** Vector3をJava配列に変換する（assertArrayEqualsに渡すため） */
+    /** Vector3をJava配列に変換する（値を比較できるようにするため） */
     private fun Vector3.toArray() = doubleArrayOf(x, y, z)
 
     @Test
-    @DisplayName("length ゼロベクトルの長さが0")
+    @DisplayName("length: ゼロベクトルの長さが0")
     fun length_zero() {
         assertThat(Vector3.zero.length).isEqualTo(0.0, offset(TOL))
     }
 
     @Test
-    @DisplayName("length XYZ軸の単位ベクトルの長さが1")
+    @DisplayName("length: XYZ軸の単位ベクトルの長さが1")
     fun length_unit() {
         assertThat(Vector3.unitX.length).isEqualTo(1.0, offset(TOL))
         assertThat(Vector3.unitY.length).isEqualTo(1.0, offset(TOL))
@@ -28,13 +28,13 @@ internal class Vector3Test {
     }
 
     @Test
-    @DisplayName("length (3, 4, 0)の長さが5")
+    @DisplayName("length: (3, 4, 0)の長さが5")
     fun length_5() {
         assertThat(Vector3(3.0, 4.0, 0.0).length).isEqualTo(5.0, offset(TOL))
     }
 
     @Test
-    @DisplayName("length 長さが1~100のベクトル")
+    @DisplayName("length: 長さが1~100のベクトル")
     fun length_1to100() {
         for (count in 1..100) {
             val len = count.toDouble()
@@ -46,109 +46,174 @@ internal class Vector3Test {
     }
 
     @Test
-    fun getUnit() {
-        assertArrayEquals(Vector3.unitX.toArray(), Vector3(2.0, 0.0, 0.0).unit.toArray(), TOL)
-        assertArrayEquals(Vector3.unitY.toArray(), Vector3(0.0, 3.0, 0.0).unit.toArray(), TOL)
-        assertArrayEquals(Vector3.unitZ.toArray(), Vector3(0.0, 0.0, 4.0).unit.toArray(), TOL)
-        for (i in 1..100) {
-            val vec = Vector3.random()
-            assertEquals(1.0, vec.unit.length, TOL)
-        }
+    @DisplayName("unit: XYZ軸に沿ったベクトルの単位ベクトル")
+    fun unit_xyz() {
+        assertThat(Vector3(2.0, 0.0, 0.0).unit.toArray())
+            .containsExactly(Vector3.unitX.toArray(), offset(TOL))
+        assertThat(Vector3(0.0, 3.0, 0.0).unit.toArray())
+            .containsExactly(Vector3.unitY.toArray(), offset(TOL))
+        assertThat(Vector3(0.0, 0.0, 4.0).unit.toArray())
+            .containsExactly(Vector3.unitZ.toArray(), offset(TOL))
     }
 
     @Test
-    fun dot() {
-        assertEquals(1.0, Vector3.unitX dot Vector3.unitX, TOL)
-        assertEquals(1.0, Vector3.unitY dot Vector3.unitY, TOL)
-        assertEquals(1.0, Vector3.unitZ dot Vector3.unitZ, TOL)
-        assertEquals(0.0, Vector3.unitX dot Vector3.unitY, TOL)
-        assertEquals(0.0, Vector3.unitY dot Vector3.unitZ, TOL)
-        assertEquals(0.0, Vector3.unitZ dot Vector3.unitX, TOL)
+    @DisplayName("unit: ランダムなベクトルの単位ベクトルの長さが1")
+    fun unit_random() {
         for (count in 1..100) {
             val vec = Vector3.random()
-            assertEquals(vec.length, sqrt(vec dot vec), TOL)
-            assertEquals(vec.length, (vec dot vec.unit), TOL)
+            assertThat(vec.unit.length).isEqualTo(1.0, offset(TOL))
         }
     }
 
     @Test
-    fun cross() {
-        assertArrayEquals(Vector3.zero.toArray(), (Vector3.unitX cross Vector3.unitX).toArray(), TOL)
-        assertArrayEquals(Vector3.zero.toArray(), (Vector3.unitY cross Vector3.unitY).toArray(), TOL)
-        assertArrayEquals(Vector3.zero.toArray(), (Vector3.unitZ cross Vector3.unitZ).toArray(), TOL)
-        assertArrayEquals(Vector3.unitZ.toArray(), (Vector3.unitX cross Vector3.unitY).toArray(), TOL)
-        assertArrayEquals(Vector3.unitX.toArray(), (Vector3.unitY cross Vector3.unitZ).toArray(), TOL)
-        assertArrayEquals(Vector3.unitY.toArray(), (Vector3.unitZ cross Vector3.unitX).toArray(), TOL)
-        assertArrayEquals((-Vector3.unitZ).toArray(), (Vector3.unitY cross Vector3.unitX).toArray(), TOL)
-        assertArrayEquals((-Vector3.unitX).toArray(), (Vector3.unitZ cross Vector3.unitY).toArray(), TOL)
-        assertArrayEquals((-Vector3.unitY).toArray(), (Vector3.unitX cross Vector3.unitZ).toArray(), TOL)
-        // クロス積からさらにクロス積を求めて元のベクトルに戻ることをチェックする
+    @DisplayName("dot: XYZ軸の単位ベクトルの自身との内積が1")
+    fun dot_xyz1() {
+        assertThat(Vector3.unitX dot Vector3.unitX).isEqualTo(1.0, offset(TOL))
+        assertThat(Vector3.unitY dot Vector3.unitY).isEqualTo(1.0, offset(TOL))
+        assertThat(Vector3.unitY dot Vector3.unitY).isEqualTo(1.0, offset(TOL))
+    }
+
+    @Test
+    @DisplayName("dot: XYZ軸の2つの単位ベクトルの内積が0")
+    fun dot_xyz2() {
+        assertThat(Vector3.unitX dot Vector3.unitY).isEqualTo(0.0, offset(TOL))
+        assertThat(Vector3.unitY dot Vector3.unitZ).isEqualTo(0.0, offset(TOL))
+        assertThat(Vector3.unitZ dot Vector3.unitX).isEqualTo(0.0, offset(TOL))
+    }
+
+    @Test
+    @DisplayName("dot: ランダムなベクトルの自信との内積が長さの2乗に等しい")
+    fun dot_random1() {
+        val vec = Vector3.random()
+        assertThat(vec dot vec).isEqualTo(vec.length * vec.length, offset(TOL))
+    }
+
+    @Test
+    @DisplayName("dot: ランダムなベクトルと自身の単位ベクトルとの内積が長さに等しい")
+    fun dot_random2() {
+        val vec = Vector3.random()
+        assertThat(vec dot vec.unit).isEqualTo(vec.length, offset(TOL))
+    }
+
+    @Test
+    @DisplayName("cross: XYZ軸の単位ベクトルの自身との外積がゼロベクトル")
+    fun cross_xyz1() {
+        assertThat((Vector3.unitX cross Vector3.unitX).toArray())
+            .containsExactly(Vector3.zero.toArray(), offset(TOL))
+        assertThat((Vector3.unitY cross Vector3.unitY).toArray())
+            .containsExactly(Vector3.zero.toArray(), offset(TOL))
+        assertThat((Vector3.unitZ cross Vector3.unitZ).toArray())
+            .containsExactly(Vector3.zero.toArray(), offset(TOL))
+    }
+
+    @Test
+    @DisplayName("cross: XYZ軸の2つの単位ベクトルの外積が残り1つのベクトル")
+    fun cross_xyz2() {
+        assertThat((Vector3.unitX cross Vector3.unitY).toArray())
+            .containsExactly(Vector3.unitZ.toArray(), offset(TOL))
+        assertThat((Vector3.unitY cross Vector3.unitZ).toArray())
+            .containsExactly(Vector3.unitX.toArray(), offset(TOL))
+        assertThat((Vector3.unitZ cross Vector3.unitX).toArray())
+            .containsExactly(Vector3.unitY.toArray(), offset(TOL))
+        assertThat((Vector3.unitY cross Vector3.unitX).toArray())
+            .containsExactly((-Vector3.unitZ).toArray(), offset(TOL))
+        assertThat((Vector3.unitZ cross Vector3.unitY).toArray())
+            .containsExactly((-Vector3.unitX).toArray(), offset(TOL))
+        assertThat((Vector3.unitX cross Vector3.unitZ).toArray())
+            .containsExactly((-Vector3.unitY).toArray(), offset(TOL))
+    }
+
+    @Test
+    @DisplayName("cross: ランダムなベクトル2つのクロス積からさらにクロス積を求めて元のベクトルに戻る")
+    fun cross_random1() {
         for (count in 1..100) {
             val i = Vector3.random().unit
             val j = Vector3.random().unit
             val k = (i cross j).unit
             val jOrtho = k cross i
-            assertEquals(0.0, (jOrtho dot i), TOL)
-            assertArrayEquals(i.toArray(), (jOrtho cross k).toArray(), TOL)
+            assertThat(jOrtho dot i).isEqualTo(0.0, offset(TOL))
+            assertThat((jOrtho cross k).toArray()).containsExactly(i.toArray(), offset(TOL))
         }
-        // クロス積の長さをチェックする
+    }
+
+    @Test
+    @DisplayName("cross: ランダムなベクトル2つのクロス積の長さがそれぞれの長さの積にsin(θ)をかけた値")
+    fun cross_random2() {
         for (count in 1..100) {
             val vec1 = Vector3.random()
             val vec2 = Vector3.random()
             val angle = vec1.getAngle(vec2)
-            assertEquals(
-                vec1.length * vec2.length * sin(angle),
-                (vec1 cross vec2).length,
-                TOL
-            )
+            assertThat((vec1 cross vec2).length)
+                .isEqualTo(vec1.length * vec2.length * sin(angle), offset(TOL))
         }
     }
 
     @Test
-    fun getAngle() {
-        assertEquals(0.0, Vector3.unitX.getAngle(Vector3.unitX), TOL)
-        assertEquals(PI, Vector3.unitX.getAngle(-Vector3.unitX), TOL)
-        assertEquals(PI / 2, Vector3.unitX.getAngle(Vector3.unitY), TOL)
-        assertEquals(PI / 2, Vector3.unitY.getAngle(Vector3.unitZ), TOL)
-        assertEquals(PI / 2, Vector3.unitZ.getAngle(Vector3.unitX), TOL)
-        assertEquals(PI / 2, Vector3.unitY.getAngle(Vector3.unitX), TOL)
-        assertEquals(PI / 2, Vector3.unitZ.getAngle(Vector3.unitY), TOL)
-        assertEquals(PI / 2, Vector3.unitX.getAngle(Vector3.unitZ), TOL)
+    @DisplayName("getAngle: XYZ軸の単位ベクトルの自身とのなす角度が0")
+    fun getAngle_xyz() {
+        assertThat(Vector3.unitX.getAngle(Vector3.unitX)).isEqualTo(0.0, offset(TOL))
+        assertThat(Vector3.unitY.getAngle(Vector3.unitY)).isEqualTo(0.0, offset(TOL))
+        assertThat(Vector3.unitZ.getAngle(Vector3.unitZ)).isEqualTo(0.0, offset(TOL))
+    }
+
+    @Test
+    @DisplayName("getAngle: XYZ軸の単位ベクトルの自身の逆ベクトルとのなす角度がπ")
+    fun getAngle_xyz2() {
+        assertThat(Vector3.unitX.getAngle(-Vector3.unitX)).isEqualTo(PI, offset(TOL))
+        assertThat(Vector3.unitY.getAngle(-Vector3.unitY)).isEqualTo(PI, offset(TOL))
+        assertThat(Vector3.unitZ.getAngle(-Vector3.unitZ)).isEqualTo(PI, offset(TOL))
+    }
+
+    @Test
+    @DisplayName("getAngle: XYZ軸の2つの単位ベクトルのなす角度がπ/2")
+    fun getAngle_xyz3() {
+        assertThat(Vector3.unitX.getAngle(Vector3.unitY)).isEqualTo(PI / 2, offset(TOL))
+        assertThat(Vector3.unitY.getAngle(Vector3.unitZ)).isEqualTo(PI / 2, offset(TOL))
+        assertThat(Vector3.unitZ.getAngle(Vector3.unitX)).isEqualTo(PI / 2, offset(TOL))
+        assertThat(Vector3.unitY.getAngle(Vector3.unitX)).isEqualTo(PI / 2, offset(TOL))
+        assertThat(Vector3.unitZ.getAngle(Vector3.unitY)).isEqualTo(PI / 2, offset(TOL))
+        assertThat(Vector3.unitX.getAngle(Vector3.unitZ)).isEqualTo(PI / 2, offset(TOL))
+
+    }
+
+    @Test
+    @DisplayName("getAngle: ランダムなベクトル2つのなす角度から求めた内積の値が正しい")
+    fun getAngle_random() {
         for (count in 1..100) {
             val vec1 = Vector3.random()
             val vec2 = Vector3.random()
-            assertEquals(
-                vec1 dot vec2,
-                vec1.length * vec2.length * cos(vec1.getAngle(vec2)),
-                TOL
-            )
+            val actual = vec1.length * vec2.length * cos(vec1.getAngle(vec2))
+            assertThat(actual).isEqualTo(vec1 dot vec2, offset(TOL))
         }
     }
 
     @Test
-    fun getParallel() {
+    @DisplayName("getParallel: ランダムなベクトル2つの平行成分による外積がゼロベクトル")
+    fun getParallel_random() {
         for (count in 1..100) {
             val vec1 = Vector3.random()
             val vec2 = Vector3.random()
-            assertArrayEquals(
-                Vector3.zero.toArray(),
-                (vec1.getParallel(vec2) cross vec2).toArray(),
-                TOL
-            )
+            assertThat((vec1.getParallel(vec2) cross vec2).toArray())
+                .containsExactly(Vector3.zero.toArray(), offset(TOL))
         }
     }
 
     @Test
-    fun getPerpendicular() {
+    @DisplayName("getPerpendicular: ランダムなベクトル2つの垂直成分による内積が0")
+    fun getPerpendicular_random1() {
         for (count in 1..100) {
             val vec1 = Vector3.random()
             val vec2 = Vector3.random()
-            assertEquals(0.0, vec1.getPerpendicular(vec2) dot vec2, TOL)
-            assertArrayEquals(
-                (vec1 cross vec2).toArray(),
-                (vec1.getPerpendicular(vec2) cross vec2).toArray(),
-                TOL
-            )
+            assertThat(vec1.getPerpendicular(vec2) dot vec2).isEqualTo(0.0, offset(TOL))
         }
+    }
+
+    @Test
+    @DisplayName("getPerpendicular: ランダムなベクトル2つの外積が垂直成分による外積に等しい")
+    fun getPerpendicular_random2() {
+        val vec1 = Vector3.random()
+        val vec2 = Vector3.random()
+        assertThat((vec1.getPerpendicular(vec2) cross vec2).toArray())
+            .containsExactly((vec1 cross vec2).toArray(), offset(TOL))
     }
 }
