967f02cb2cdbefdc86d0b6f67d7a107da088aee8
yin-yang-alchemist <nakamoto.masa@gmail.com>
2019-02-27 23:03:19 +0900

    Vector3のテストメソッドの一部をParameterizedTestとRepeatedTestに置き換え

--- src/test/kotlin/Vector3Test.kt
+++ src/test/kotlin/Vector3Test.kt
@@ -2,6 +2,9 @@ import kotlin.math.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.RepeatedTest
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.offset
 
@@ -27,10 +30,17 @@ internal class Vector3Test {
         assertThat(Vector3.unitZ.length).isEqualTo(1.0, offset(TOL))
     }
 
-    @Test
-    @DisplayName("length: (3, 4, 0)の長さが5")
-    fun length_5() {
-        assertThat(Vector3(3.0, 4.0, 0.0).length).isEqualTo(5.0, offset(TOL))
+    @ParameterizedTest
+    @DisplayName("length: 長さの値が整数になるベクトル")
+    @CsvSource(
+        "3.0, 4.0, 0.0, 5.0",
+        "2.0, 1.0, 2.0, 3.0",
+        "4.0, 4.0, 7.0, 9.0",
+        "5.0, 0.0, 12.0, 13.0"
+    )
+    fun length_parameterized(x: Double, y: Double, z: Double, expected: Double) {
+        val vec = Vector3(x, y, z)
+        assertThat(vec.length).isEqualTo(expected, offset(TOL))
     }
 
     @Test
@@ -56,13 +66,11 @@ internal class Vector3Test {
             .containsExactly(Vector3.unitZ.toArray(), offset(TOL))
     }
 
-    @Test
+    @RepeatedTest(100)
     @DisplayName("unit: ランダムなベクトルの単位ベクトルの長さが1")
     fun unit_random() {
-        for (count in 1..100) {
-            val vec = Vector3.random()
-            assertThat(vec.unit.length).isEqualTo(1.0, offset(TOL))
-        }
+        val vec = Vector3.random()
+        assertThat(vec.unit.length).isEqualTo(1.0, offset(TOL))
     }
 
     @Test
@@ -73,7 +81,6 @@ internal class Vector3Test {
         assertThat(Vector3.unitY dot Vector3.unitY).isEqualTo(1.0, offset(TOL))
     }
 
-    @Test
     @DisplayName("dot: XYZ軸の2つの単位ベクトルの内積が0")
     fun dot_xyz2() {
         assertThat(Vector3.unitX dot Vector3.unitY).isEqualTo(0.0, offset(TOL))
@@ -81,14 +88,14 @@ internal class Vector3Test {
         assertThat(Vector3.unitZ dot Vector3.unitX).isEqualTo(0.0, offset(TOL))
     }
 
-    @Test
+    @RepeatedTest(100)
     @DisplayName("dot: ランダムなベクトルの自信との内積が長さの2乗に等しい")
     fun dot_random1() {
         val vec = Vector3.random()
         assertThat(vec dot vec).isEqualTo(vec.length * vec.length, offset(TOL))
     }
 
-    @Test
+    @RepeatedTest(100)
     @DisplayName("dot: ランダムなベクトルと自身の単位ベクトルとの内積が長さに等しい")
     fun dot_random2() {
         val vec = Vector3.random()
@@ -123,29 +130,25 @@ internal class Vector3Test {
             .containsExactly((-Vector3.unitY).toArray(), offset(TOL))
     }
 
-    @Test
+    @RepeatedTest(100)
     @DisplayName("cross: ランダムなベクトル2つのクロス積からさらにクロス積を求めて元のベクトルに戻る")
     fun cross_random1() {
-        for (count in 1..100) {
-            val i = Vector3.random().unit
-            val j = Vector3.random().unit
-            val k = (i cross j).unit
-            val jOrtho = k cross i
-            assertThat(jOrtho dot i).isEqualTo(0.0, offset(TOL))
-            assertThat((jOrtho cross k).toArray()).containsExactly(i.toArray(), offset(TOL))
-        }
+        val i = Vector3.random().unit
+        val j = Vector3.random().unit
+        val k = (i cross j).unit
+        val jOrtho = k cross i
+        assertThat(jOrtho dot i).isEqualTo(0.0, offset(TOL))
+        assertThat((jOrtho cross k).toArray()).containsExactly(i.toArray(), offset(TOL))
     }
 
-    @Test
+    @RepeatedTest(100)
     @DisplayName("cross: ランダムなベクトル2つのクロス積の長さがそれぞれの長さの積にsin(θ)をかけた値")
     fun cross_random2() {
-        for (count in 1..100) {
-            val vec1 = Vector3.random()
-            val vec2 = Vector3.random()
-            val angle = vec1.getAngle(vec2)
-            assertThat((vec1 cross vec2).length)
-                .isEqualTo(vec1.length * vec2.length * sin(angle), offset(TOL))
-        }
+        val vec1 = Vector3.random()
+        val vec2 = Vector3.random()
+        val angle = vec1.getAngle(vec2)
+        assertThat((vec1 cross vec2).length)
+            .isEqualTo(vec1.length * vec2.length * sin(angle), offset(TOL))
     }
 
     @Test
@@ -176,39 +179,33 @@ internal class Vector3Test {
 
     }
 
-    @Test
+    @RepeatedTest(100)
     @DisplayName("getAngle: ランダムなベクトル2つのなす角度から求めた内積の値が正しい")
     fun getAngle_random() {
-        for (count in 1..100) {
-            val vec1 = Vector3.random()
-            val vec2 = Vector3.random()
-            val actual = vec1.length * vec2.length * cos(vec1.getAngle(vec2))
-            assertThat(actual).isEqualTo(vec1 dot vec2, offset(TOL))
-        }
+        val vec1 = Vector3.random()
+        val vec2 = Vector3.random()
+        val actual = vec1.length * vec2.length * cos(vec1.getAngle(vec2))
+        assertThat(actual).isEqualTo(vec1 dot vec2, offset(TOL))
     }
 
-    @Test
+    @RepeatedTest(100)
     @DisplayName("getParallel: ランダムなベクトル2つの平行成分による外積がゼロベクトル")
     fun getParallel_random() {
-        for (count in 1..100) {
-            val vec1 = Vector3.random()
-            val vec2 = Vector3.random()
-            assertThat((vec1.getParallel(vec2) cross vec2).toArray())
-                .containsExactly(Vector3.zero.toArray(), offset(TOL))
-        }
+        val vec1 = Vector3.random()
+        val vec2 = Vector3.random()
+        assertThat((vec1.getParallel(vec2) cross vec2).toArray())
+            .containsExactly(Vector3.zero.toArray(), offset(TOL))
     }
 
-    @Test
+    @RepeatedTest(100)
     @DisplayName("getPerpendicular: ランダムなベクトル2つの垂直成分による内積が0")
     fun getPerpendicular_random1() {
-        for (count in 1..100) {
-            val vec1 = Vector3.random()
-            val vec2 = Vector3.random()
-            assertThat(vec1.getPerpendicular(vec2) dot vec2).isEqualTo(0.0, offset(TOL))
-        }
+        val vec1 = Vector3.random()
+        val vec2 = Vector3.random()
+        assertThat(vec1.getPerpendicular(vec2) dot vec2).isEqualTo(0.0, offset(TOL))
     }
 
-    @Test
+    @RepeatedTest(100)
     @DisplayName("getPerpendicular: ランダムなベクトル2つの外積が垂直成分による外積に等しい")
     fun getPerpendicular_random2() {
         val vec1 = Vector3.random()
